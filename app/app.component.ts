fe10e963021e8bddf0914149739049fe3fcb6b21
Andrew Murray <sckoobs@gmail.com>
2016-04-26 21:49:50 +0100

    Starting to add routing with a product detail view

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "angular2/core";
 import { HTTP_PROVIDERS } from "angular2/http";
 import 'rxjs/Rx';
+import { ROUTER_PROVIDERS } from "angular2/router";
 import { ProductListComponent } from "./products/product-list.component";
 import { ProductService } from "./products/product.service";
 
@@ -12,7 +13,7 @@ import { ProductService } from "./products/product.service";
             <pm-products></pm-products>
         </div>`,
     directives: [ProductListComponent],
-    providers: [ProductService, HTTP_PROVIDERS]
+    providers: [ProductService, HTTP_PROVIDERS, ROUTER_PROVIDERS]
 })
 export class AppComponent {
     pageTitle: string = "Acme Product Management"
---
+++ app/products/product-detail.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "angular2/core";
+
+@Component({
+    templateUrl: "app/products/product-detail.component.html"
+})
+export class ProductDetailCompoent {
+    pageTitle: string = "Product Detail";
+}
