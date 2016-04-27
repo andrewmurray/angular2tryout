de3e2998acae7505c5788986e7396fa8ef6f10bf
Andrew Murray <sckoobs@gmail.com>
2016-04-27 21:33:40 +0100

    Adding routing configuration

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "angular2/core";
 import { HTTP_PROVIDERS } from "angular2/http";
 import 'rxjs/Rx';
-import { ROUTER_PROVIDERS } from "angular2/router";
+import { ROUTER_PROVIDERS, RouteConfig } from "angular2/router";
 import { ProductListComponent } from "./products/product-list.component";
 import { ProductService } from "./products/product.service";
+import { WelcomeComponent } from "./home/welcome.component";
 
 @Component({
     selector: "pm-app",
@@ -15,6 +16,10 @@ import { ProductService } from "./products/product.service";
     directives: [ProductListComponent],
     providers: [ProductService, HTTP_PROVIDERS, ROUTER_PROVIDERS]
 })
+@RouteConfig([
+    { path: "/welcome", name: "Welcome", component: WelcomeComponent, useAsDefault: true},
+    { path: "/products", name: "Products", component: ProductListComponent},
+])
 export class AppComponent {
     pageTitle: string = "Acme Product Management"
 }
