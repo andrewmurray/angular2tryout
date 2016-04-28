fcf6571da67c5a747232990d6298299e79c38b2d
Andrew Murray <sckoobs@gmail.com>
2016-04-28 16:47:40 +0100

    Added application menu and routing now works

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,15 @@
 import { Component } from "angular2/core";
 import { HTTP_PROVIDERS } from "angular2/http";
 import 'rxjs/Rx';
-import { ROUTER_PROVIDERS, RouteConfig } from "angular2/router";
+import { ROUTER_PROVIDERS, RouteConfig, ROUTER_DIRECTIVES } from "angular2/router";
 import { ProductListComponent } from "./products/product-list.component";
 import { ProductService } from "./products/product.service";
 import { WelcomeComponent } from "./home/welcome.component";
 
 @Component({
     selector: "pm-app",
-    template: `
-        <div>
-            <h1>{{pageTitle}}</h1>
-            <pm-products></pm-products>
-        </div>`,
-    directives: [ProductListComponent],
+    templateUrl: "app/app.component.html",
+    directives: [ROUTER_DIRECTIVES],
     providers: [ProductService, HTTP_PROVIDERS, ROUTER_PROVIDERS]
 })
 @RouteConfig([
